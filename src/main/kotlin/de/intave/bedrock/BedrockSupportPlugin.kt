b522abd4cf4e8e70f405d00dea39ffd4e5d257c4
Lennox <lennox2508@gmail.com>
2022-06-14 21:16:08 +0200

    Add re-checking for floodgate api change

--- src/main/kotlin/de/intave/bedrock/BedrockSupportPlugin.kt
+++ src/main/kotlin/de/intave/bedrock/BedrockSupportPlugin.kt
@@ -1,10 +1,10 @@
 package de.intave.bedrock
 
 import de.jpx3.intave.IntaveAccessor
-import de.jpx3.intave.access.IntaveAccess
 import de.jpx3.intave.access.IntaveColdException
 import de.jpx3.intave.access.player.trust.TrustFactor
 import org.bukkit.Bukkit
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -12,7 +12,7 @@ import org.bukkit.plugin.java.JavaPlugin
 import org.geysermc.floodgate.api.FloodgateApi
 
 class BedrockSupportPlugin : JavaPlugin(), Listener {
-    private lateinit var access: IntaveAccess
+    private var accessReference = IntaveAccessor.weakAccess()
 
     override fun onEnable() {
         enableBedrockSupport()
@@ -23,16 +23,31 @@ class BedrockSupportPlugin : JavaPlugin(), Listener {
      * to bedrock players
      */
     private fun enableBedrockSupport() {
-        access = IntaveAccessor.weakAccess().get() ?: throw IntaveColdException("Intave offline")
         Bukkit.getPluginManager().registerEvents(this, this)
     }
 
     @EventHandler
     private fun receiveJoin(event: PlayerJoinEvent) {
-        val player = event.player
+        check(0, event.player)
+    }
+
+    /**
+     * Checks if the player's floodgate state changes in the next 30 ticks (3 x 10 ticks) to re-assign the
+     * trust-factor if anything went wrong
+     *
+     * Once the player is marked as bedrock player no re-check will be scheduled
+     *
+     * @param tick The current ticks
+     * @param player The player being checked
+     */
+    private fun check(tick: Int, player: Player) {
+        val access = accessReference.get() ?: throw IntaveColdException("Intave offline")
         val intavePlayer = access.player(player)
         if (FloodgateApi.getInstance().isFloodgatePlayer(player.uniqueId)) {
             intavePlayer.setTrustFactor(TrustFactor.BYPASS)
+        } else if (tick < 3) {
+            // Schedule a new check in 10 ticks
+            Bukkit.getScheduler().scheduleSyncDelayedTask(this, { check(tick + 1, player) }, 10)
         }
     }
 }
